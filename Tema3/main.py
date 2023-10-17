595e693a13e7c71f82db11bf9d8c91474ee62f1c
AdascaliteiRobert <100295184+AdascaliteiRobert@users.noreply.github.com>
2023-10-17 12:26:32 +0300

    main.py

--- Tema3/main.py
+++ Tema3/main.py
@@ -2,28 +2,28 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
 
-# Definim structura rețelei bayesiene
-model = BayesianNetwork([('Cutremur', 'Incendiu'), ('Cutremur', 'Alarmă'), ('Incendiu', 'Alarmă')])
+# Definim structura retelei bayesiene
+model = BayesianNetwork([('Cutremur', 'Incendiu'), ('Cutremur', 'Alarma'), ('Incendiu', 'Alarma')])
 
-# Definim Tabelele de probabilități condiționate (CPD)
+# Definim Tabelele de probabilitati conditionate (CPD)
 cpd_cutremur = TabularCPD(variable='Cutremur', variable_card=2, values=[[0.9995], [0.0005]])
 cpd_incendiu = TabularCPD(variable='Incendiu', variable_card=2, values=[[0.99, 0.97], [0.01, 0.03]], evidence=['Cutremur'], evidence_card=[2])
-cpd_alarmă = TabularCPD(variable='Alarmă', variable_card=2, values=[[0.9999, 0.05, 0.98 , 0.02], [0.0001, 0.95, 0.02, 0.98]], evidence=['Incendiu', 'Cutremur'], evidence_card=[2, 2])
+cpd_alarma = TabularCPD(variable='Alarma', variable_card=2, values=[[0.9999, 0.05, 0.98 , 0.02], [0.0001, 0.95, 0.02, 0.98]], evidence=['Incendiu', 'Cutremur'], evidence_card=[2, 2])
 
-# Adăugăm CPD-urile la rețeaua bayesiană
+# Adaugam CPD-urile la reteaua bayesiana
 model.add_cpds(cpd_cutremur, cpd_incendiu, cpd_alarmă)
 
-# Verificăm dacă rețeaua bayesiană este validă
+# Verificam daca reteaua bayesiana este valida
 model.check_model()
 
-# Creăm un obiect pentru inferență
+# Cream un obiect pentru inferenta
 inference = VariableElimination(model)
 
-# Calculăm probabilitatea că a avut loc un cutremur, dat fiind că alarma de incendiu a fost declanșată
-result = inference.query(variables=['Cutremur'], evidence={'Alarmă': 1})
+# Calculam probabilitatea ca a avut loc un cutremur, dat fiind ca alarma de incendiu a fost declansata
+result = inference.query(variables=['Cutremur'], evidence={'Alarma': 1})
 
 print(result)
 
-result = inference.query(variables=['Incendiu'] , evidence={'Alarmă':0})
+result = inference.query(variables=['Incendiu'] , evidence={'Alarma':0})
 
 print(result)
